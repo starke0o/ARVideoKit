ee73453dbe8cc9662db979cb0644bf5f86e9a041
Johannes Starke <johannes.starke@develappers.de>
2019-11-23 16:54:33 +0100

    spm: added target and product ARVideoKit

--- Package.swift
+++ Package.swift
@@ -1,8 +1,19 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "ARVideoKit"
+    name: "ARVideoKit",
+    products: [
+        .library(
+            name: "ARVideoKit",
+            targets: ["ARVideoKit"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "ARVideoKit",
+            dependencies: [])
+    ]
 )
